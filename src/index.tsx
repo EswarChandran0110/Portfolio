428a6083c8ace15815b299e04612f15ef5e2bf58
Eswar_Chandran <82373432+EswarChandran0110@users.noreply.github.com>
2026-08-19 13:14:51 +0530

    Initialize React app with Redux provider

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "@/App";
+import "./styles/globals.css";
+import { store } from "./store";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
